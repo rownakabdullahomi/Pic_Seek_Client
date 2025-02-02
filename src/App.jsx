4d6cd3b51ccdf0e8da5b4da005106347441f1525
rownakabdullahomi <rownakabdullahomi@gmail.com>
2025-02-03 01:41:52 +0600

    initial setup done

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Route, Routes } from "react-router";
+import Home from "./pages/Home";
+import Root from "./layouts/Root";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Root></Root>}>
+        <Route index element={<Home></Home>}></Route>
+      </Route>
+    </Routes>
+  );
+}
+
+export default App;
---
+++ src/layouts/Root.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router";
+
+
+const Root = () => {
+    return (
+        <div>
+            Root
+            <Outlet/>
+        </div>
+    );
+};
+
+export default Root;
