ae3065f8716fb25828e1cd5b77f5e722150e8f66
rownakabdullahomi <rownakabdullahomi@gmail.com>
2025-02-03 17:19:01 +0600

    banner added

---
+++ src/components/Banner.jsx
@@ -0,0 +1,28 @@
+import { Link } from "react-router";
+
+const Banner = () => {
+  return (
+    <div
+      className="hero h-[500px]"
+      style={{
+        backgroundImage:
+          "url(https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp)",
+      }}
+    >
+      <div className="hero-overlay bg-opacity-60"></div>
+      <div className="hero-content text-neutral-content text-center">
+        <div className="max-w-md">
+          <h1 className="mb-5 text-5xl font-bold">Hello I am Pic Seek</h1>
+          <p className="mb-5">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
+          <Link to={"/generate"} className="btn btn-primary">Generate Image</Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
